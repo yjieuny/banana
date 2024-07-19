77784cdccf07980de63a385a10e8899af057f50f
yjieuny <asd654@hanmail.net>
2024-07-19 18:14:05 +0900

    feat: Remove store service layer

--- api/controllers/storeController.js
+++ api/controllers/storeController.js
@@ -1,9 +1,9 @@
 const { catchAsync } = require('../utils/error');
-const viewService = require('../services/storeService');
+const storeDao = require('../models/storeDao');
 
 const getStoreView = catchAsync(async (req, res) => {
   const storeId = req.params.storeId;
-  const store = await viewService.getStoresAndReviews(storeId);
+  const store = await storeDao.getStoresAndReviews(storeId);
   res.status(201).json({ data: store });
 });
 
--- api/services/storeService.js
+++
@@ -1,7 +0,0 @@
-const storeDao = require('../models/storeDao');
-
-const getStoresAndReviews = async (reviewId) => {
-  return await storeDao.getStoresAndReviews(reviewId);
-};
-
-module.exports = { getStoresAndReviews };
